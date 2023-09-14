df489e6ac55b303f3c02eedf6dacbe1c1bd946c9
Adarsh <singhadarsh100@gmail.com>
2023-09-15 01:02:09 +0530

    first commit

---
+++ post.php
@@ -0,0 +1,220 @@
+<?php
+function load_post()
+{
+    include "database.php";
+
+    $sql = "SELECT * FROM posts";
+
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) < 0) {
+        return null;
+    }
+    $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    mysqli_close($conn);
+
+    return $rows;
+}
+function load_post_sort_by($key, $desc = false)
+{
+    include "database.php";
+
+    $sql = "SELECT * FROM posts ORDER BY " . $key . ($desc ? " DESC;" : ";");
+
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) < 0) {
+        return null;
+    }
+    $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    mysqli_close($conn);
+
+    return $rows;
+}
+function load_post_by_category($category)
+{
+    include "database.php";
+
+    $sql = "SELECT * FROM posts WHERE category = '" . $category . "';";
+
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) < 0) {
+        return null;
+    }
+    $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    mysqli_close($conn);
+
+    return $rows;
+}
+function load_post_by_user_id($user_id)
+{
+    include "database.php";
+
+    $sql = "SELECT * FROM posts WHERE author_id = '" . $user_id . "';";
+
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) < 0) {
+        return null;
+    }
+    $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    mysqli_close($conn);
+
+    return $rows;
+}
+function load_post_detail($post_id)
+{
+    include "database.php";
+    $sql = "SELECT * FROM posts WHERE id = '" . $post_id . "'";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) < 0) {
+        return null;
+    }
+    $row = mysqli_fetch_assoc($result);
+    mysqli_close($conn);
+    return $row;
+}
+function update_post_view($post_id, $current_view)
+{
+    include "database.php";
+    $sql = "UPDATE posts SET views= '" . ($current_view + 1) . "' WHERE id = '" . $post_id . "'";
+    $result = mysqli_query($conn, $sql);
+    if (!$result) {
+        echo "VIEW_UPDATE_ERROR";
+    }
+}
+function get_username($user_id)
+{
+    include "database.php";
+    $sql = "SELECT username FROM users WHERE id = '" . $user_id . "'";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) < 0) {
+        return null;
+    }
+    $row = mysqli_fetch_assoc($result);
+    mysqli_close($conn);
+    return $row["username"];
+}
+// function get_post_comment($post_id)
+// {
+//     include "database.php";
+//     $sql = "SELECT * FROM post_comments WHERE post_id = '" . $post_id . "'";
+//     $result = mysqli_query($conn, $sql);
+//     if (mysqli_num_rows($result) < 0) {
+//         return null;
+//     }
+//     $row = mysqli_fetch_all($result, MYSQLI_ASSOC);
+//     mysqli_close($conn);
+//     return $row;
+// }
+// function set_comment()
+// {
+//     include "database.php";
+//     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//         login_required();
+//         $comment = $_POST["userCommentInput"];
+//         if (empty($comment))
+//             return "COMMENT_EMPTY_FAIL";
+//         $sql = "INSERT INTO post_comments(user_id, post_id, use_comment) VALUES ('" . $_SESSION["user_id"] . "','" . $_GET["id"] . "','" . $comment . "')";
+//         $result = mysqli_query($conn, $sql);
+//         if (!$result)
+//             return "COMMENT_UNKNOWN_FAIL";
+//         else {
+//             $url = "Location: ./view-post.php?id=" . $_GET["id"];
+//             header($url);
+//         }
+//     }
+// }
+function create_post()
+{
+    include "database.php";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $sql = "SELECT id FROM posts ORDER BY id DESC LIMIT 1";
+        $id_post = 0;
+        $result = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_assoc($result);
+        $id_post = $row["id"] + 1;
+        $sql = "INSERT INTO posts(id, author_id, title, body, category) VALUES ('" . $id_post . "', '" . $_SESSION["user_id"] . "', '" . $_POST["post-title"] . "', '" . $_POST["post-body"] . "', '" . $_POST["post-category"] . "')";
+        $result = mysqli_query($conn, $sql);
+        mysqli_close($conn);
+        if (!$result) {
+            return "CREATE_POST_FAIL";
+        } else {
+            $location = "Location: ./index.php";
+            header($location);
+        }
+    }
+}
+
+function vote_score($post_id, $type)
+{
+    if (!login_required()) {
+        include "database.php";
+        $sql = "SELECT scores FROM posts WHERE id = '" . $post_id . "'";
+        $result = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_assoc($result);
+        $score = $row["scores"];
+        if ($type) {
+            $score = $score + 1;
+        } else {
+            $score = $score - 1;
+        }
+        $sql = "UPDATE posts SET scores='" . $score . "' WHERE id = '" . $post_id . "'";
+        $result = mysqli_query($conn, $sql);
+        mysqli_close($conn);
+        if (!$result) {
+            return "VOTE_FAIL";
+        } else {
+            $url = "Location: ./view-post.php?id=" . $post_id;
+            header($url);
+        }
+    }
+}
+
+function update_post($post_id)
+{
+    include "database.php";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $sql = "UPDATE posts SET title='" . $_POST["post-title"] . "',body='" . $_POST["post-body"] . "',category=' " . $_POST["post-category"] . "' WHERE id='" . $post_id . "'";
+        $result = mysqli_query($conn, $sql);
+        mysqli_close($conn);
+        if (!$result) {
+            return "UPDATE_POST_FAIL";
+        } else {
+            $location = "Location: ./index.php";
+            header($location);
+        }
+    }
+}
+
+function delete_post($post_id)
+{
+    include "database.php";
+    $sql = "DELETE FROM `posts` WHERE id = '" . $post_id . "'";
+    $result = mysqli_query($conn, $sql);
+    mysqli_close($conn);
+    if (!$result) {
+        return "DELETE_POST_FAIL";
+    } else {
+        $location = "Location: ./index.php";
+        echo "<script>alert('Delete post completed!')</script>";
+        header($location);
+    }
+}
+function get_category()
+{
+    include "database.php";
+    $sql = "SELECT DISTINCT category FROM posts";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) < 0) {
+        return null;
+    }
+    $row = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    mysqli_close($conn);
+    return $row;
+}
---
+++ view-post.php
@@ -0,0 +1,127 @@
+<?php
+session_start();
+error_reporting(E_ERROR | E_PARSE);
+include "auth.php";
+check_for_remembered_user();
+include "post.php";
+$post_detail = load_post_detail($_GET["id"]);
+if (!$post_detail) {
+    header("Location: ./404page.php");
+}
+update_post_view($_GET["id"], $post_detail["views"]);
+// $post_comment = get_post_comment($_GET["id"]);
+// set_comment();
+$_POST["userCommentInput"] = "";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learn PHP</title>
+    <link rel="stylesheet" href="assets\bootstrap-5.0.2-dist\css\bootstrap.css">
+    <link rel="stylesheet" href="assets\bootstrap-5.0.2-dist\css\bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="shortcut icon" href="assets/icon.png" type="image/x-icon">
+    <script src="assets/bootstrap-5.0.2-dist/js/bootstrap.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+    <link rel="stylesheet" href="assets/css/view-post.css">
+</head>
+
+<body>
+    <?php
+    include "header.php";
+    ?>
+
+    <main>
+        <div class="blog-detail card">
+            <div class="blog-detail-header">
+                <h1 class="blog-detail-header-title">
+                    <?php
+                    echo $post_detail["title"];
+                    ?>
+                </h1>
+                <div class="blog-detail-header-subtitle">
+                    <div class="blog-detail-header-created"><i class="fas fa-calendar"></i>
+                        <?php
+                        echo $post_detail["created"];
+                        ?>
+                    </div>
+                    <div class="blog-detail-header-author"><i class="fas fa-user"></i>
+                        <?php
+                        echo get_username($post_detail["author_id"]);
+                        ?>
+                    </div>
+                    <div class="blog-detail-header-view"><i class="fas fa-eye"></i>
+                        <?php
+                        echo $post_detail["views"];
+                        ?>
+                    </div>
+                </div>
+            </div>
+            <hr />
+            <div class="blog-detail-body">
+                <?php
+                echo $post_detail["body"];
+                ?>
+            </div>
+            <hr />
+            <div class="blog-detail-comment">
+                <h2 style="margin-top: 20px;"><i class="fas fa-comment-alt"></i> Comments</h2>
+                <div class="blog-detail-comment-container">
+                    <div class="blog-detail-comment-scores">
+                        <div><button type="button" id="btn-vote-up" onclick="btnVoteUpClick()"><i class="fas fa-sort-up"></i></button></div>
+                        <div class="blog-detail-comment-scores-num text-center">
+                            <?php
+                            if ($post_detail["scores"] > 0) {
+                                echo "+";
+                            }
+                            echo $post_detail["scores"];
+                            ?>
+                        </div>
+                        <div><button type="button" id="btn-vote-down" onclick="btnVoteDownClick()"><i class="fas fa-sort-down" style="size: 30px;"></i></button></div>
+                    </div>
+
+                    <div class=" blog-detail-comment-content">
+                        <div class="blog-detail-comment-input card">
+                            <form method="post">
+                                <label for="userCommentInput" class="form-label">Your comment: </label>
+                                <input type="text" class="form-control" id="userCommentInput" name="userCommentInput" aria-describedby="emailHelp" placeholder="Enter your comment" required>
+                                <button class="btn btn-primary m-2" type="submit">Comment</button>
+                            </form>
+                        </div>
+                        <div class="blog-detail-comment-list">
+                            <?php
+                            if (!$post_comment) {
+                                echo '<div class="blog-detail-comment-item card"> No comments </div>';
+                            } else {
+                                foreach ($post_comment as $comment) {
+                                    echo '<div class="blog-detail-comment-item card"><div class="blog-detail-comment-item-username"> <i class="fas fa-user"></i> ' . get_username($comment["user_id"]) . '</div> <div class="blog-detail-comment-item-text">' . $comment["use_comment"] . '</div> </div>';
+                                }
+                            }
+                            ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <?php
+    include "footer.php";
+    ?>
+    <script src="assets/js/header.js"></script>
+    <script src="assets/js/view-post.js"></script>
+    <script>
+        function btnVoteUpClick() {
+            location.href = "./vote-up.php?post_id=<?php echo $_GET["id"] ?>";
+        }
+
+        function btnVoteDownClick() {
+            location.href = "./vote-down.php?post_id=<?php echo $_GET["id"] ?>";
+        }
+    </script>
+</body>
+
+</html>
